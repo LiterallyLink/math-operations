a026becf80aa1bbc7f4f8237d6aca7ce8727ce83
Meow <zechariahpeoples@gmail.com>
2023-03-24 23:30:54 -0700

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::io;
+
+fn main() {
+    fn read_integer_input(prompt: &str) -> i128 {
+        loop {
+            let mut input = String::new();
+            println!("{}", prompt);
+
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+
+            match input.trim().parse::<i128>() {
+                Ok(num) => return num,
+                Err(_) => println!("Please enter a valid integer")
+            }
+
+        }
+    }
+
+    let num1 = read_integer_input("Please provide the first integer.");
+    let num2 = read_integer_input("Please provide the second integer.");
+
+    println!("Sum: {}", num1 + num2);
+    println!("Difference: {}", num1 - num2);
+    println!("Product {}", num1 * num2);
+    println!("Quotient: {}", num1 as f32 / num2 as f32);
+    
+
+}
