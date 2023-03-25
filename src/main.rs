d3c886930f33c30fb111cd3f4c387963ec53e6c8
Meow <zechariahpeoples@gmail.com>
2023-03-25 11:22:21 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::io;
 
 fn main() {
-    fn read_integer_input(prompt: &str) -> i128 {
+    fn read_integer_input(prompt: &str) -> f64 {
         loop {
             let mut input = String::new();
             println!("{}", prompt);
 
             io::stdin()
                 .read_line(&mut input)
-                .expect("Failed to read line");
+                .expect("Failed to read line.");
 
-            match input.trim().parse::<i128>() {
+            match input.trim().parse::<f64>() {
                 Ok(num) => return num,
-                Err(_) => println!("Please enter a valid integer")
+                Err(_) => println!("Please enter a valid integer.")
             }
 
         }
@@ -24,7 +24,7 @@ fn main() {
     println!("Sum: {}", num1 + num2);
     println!("Difference: {}", num1 - num2);
     println!("Product {}", num1 * num2);
-    println!("Quotient: {}", num1 as f32 / num2 as f32);
+    println!("Quotient: {}", num1 / num2);
     
 
 }
